d7d6c7f1a6fc5415f7c54374fc0b39e1abb7c0aa
OPHawk4 <517002938@qq.com>
2022-02-17 00:22:07 +0800

    Learn from springBoot to ThymeLeaf crud(without Mybatis)

---
+++ spring-boot-duing-03-template/src/main/java/com/duing/service/GuestServiceImp.java
@@ -0,0 +1,20 @@
+package com.duing.service;
+
+import com.duing.bean.Guest;
+import com.duing.dao.GuestDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GuestServiceImp implements GuestService{
+
+    @Autowired
+    private GuestDao guestDao;
+
+    @Override
+    public List<Guest> list() {
+        return guestDao.list();
+    }
+}
---
+++ spring-boot-duing-03-thymeleaf-crud/src/main/java/com/duing/dao/GuestDao.java
@@ -0,0 +1,44 @@
+package com.duing.dao;
+
+import com.duing.bean.Guest;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class GuestDao {
+    static List<Guest> guestList = new ArrayList<>();
+    static {
+        guestList.add(new Guest("黄晓明","店长"));
+        guestList.add(new Guest("秦海璐","财务"));
+        guestList.add(new Guest("林述巍","总厨"));
+        guestList.add(new Guest("王俊凯","经理"));
+        guestList.add(new Guest("杨紫","前台"));
+    }
+
+    public List<Guest> list(){
+        return guestList;
+    }
+
+    public void add(Guest guest){ guestList.add(guest); }
+
+    public void update(Guest newGuest){
+        Guest oldGuest = get(newGuest.getName());
+        oldGuest.setRole(newGuest.getRole());
+    }
+
+    public Guest get(String name){
+        for(Guest origin: guestList){
+            if(origin.getName().equals(name)){
+                return origin;
+            }
+        }
+        return new Guest("","");
+    }
+
+    public void delete(String name){
+        Guest guest = get(name);
+        guestList.remove(guest);
+    }
+}
